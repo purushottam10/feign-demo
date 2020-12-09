b80735915bc7b2fd66cfdcd645b5f31084717fb5
PURUSHOTTAM <purushottamn8@gmail.com>
2020-12-09 10:20:32 +0530

    first commit

---
+++ src/test/java/com/teachbrain/feignserver/FeignServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachbrain.feignserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
