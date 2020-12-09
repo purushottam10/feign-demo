ac93a4e3d41998c936e7d06ae8617fe518830133
PURUSHOTTAM <purushottamn8@gmail.com>
2020-12-09 12:20:06 +0530

    with post method

--- src/main/java/com/teachbrain/feginclients/controller/ApplicationStatusController.java
+++ src/main/java/com/teachbrain/feginclients/controller/ApplicationStatusController.java
@@ -2,6 +2,8 @@ package com.teachbrain.feginclients.controller;
 
 import com.teachbrain.feginclients.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("second")
 public class ApplicationStatusController {
 
-    @GetMapping("/status")
+    @GetMapping(value = "/status")
     public User getStatus(){
        return new User("puru","puru@gmai.com","80000000");
+    }
+
+    @PostMapping("/save")
+    public User save(@RequestBody User user){
+        return user;
     }
 }
--- src/main/java/com/teachbrain/feginclients/model/User.java
+++ src/main/java/com/teachbrain/feginclients/model/User.java
@@ -3,10 +3,12 @@ package com.teachbrain.feginclients.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String name ;
     private String email;
